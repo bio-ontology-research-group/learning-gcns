d4678becccefabd7781d0c48a2ac09a4f62cbaf0
Robert Hoehndorf <hohndor@dbn503-35-r.ibex.kaust.edu.sa>
2020-03-07 09:56:20 +0300

    updated testing, validation

--- mygcn.py
+++ mygcn.py
@@ -3,6 +3,7 @@ import csv
 import torch_geometric.utils as util
 import gzip
 import os
+import re
 from random import shuffle, randint
 import torch
 import torch.nn.functional as F
@@ -79,8 +80,8 @@ network = util.from_networkx(G)
 dataset_plain = []
 count = 0
 print("Reading data")
-for i in [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser("cancer/")) for f in fn]:
-    if str(i).find("FPKM-UQ") > -1 and count < 500:
+for i in [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser("/ibex/scratch/projects/c2014/tcga/")) for f in fn]:
+    if re.search("FPKM-UQ.txt.gz$", str(i)) and count < 1500:
         count += 1
         if count % 20 == 0:
             print(str(count) + " files read")
@@ -99,19 +100,24 @@ for i in [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser("canc
                 continue
         dataset_plain.append(localX)
 
+
 print("Normalizing data")
 # normalize
 maxval = torch.max(torch.stack(dataset_plain))
 dataset_plain = [t / maxval for t in dataset_plain]
 
+shuffle(dataset_plain)
+trainset = dataset_plain[:int((len(dataset_plain)+1)*.80)]
+testset = dataset_plain[int(len(dataset_plain)*.80+1):int(len(dataset_plain)*.90)]
+validationset = dataset_plain[int(len(dataset_plain)*.90+1):]
 
 # define the model layout -> use custom GCN model defined above
 
 class Net(torch.nn.Module):
     def __init__(self, edge_index, num_nodes):
         super(Net, self).__init__()
-        self.conv1 = MyGCNConv(1, FILTERS, edge_index, num_nodes, node_dim = 1)
-        self.conv2 = MyGCNConv(FILTERS, FILTERS, edge_index, num_nodes, node_dim = 1)
+        self.conv1 = MyGCNConv(1, 2 * FILTERS, edge_index, num_nodes, node_dim = 1)
+        self.conv2 = MyGCNConv(2 * FILTERS, FILTERS, edge_index, num_nodes, node_dim = 1)
         self.conv3 = MyGCNConv(FILTERS, 1, edge_index, num_nodes, node_dim = 1 )
 
     def forward(self, data):
@@ -127,7 +133,7 @@ class Net(torch.nn.Module):
         return out
 
 
-# generate masks; TODO: add validation
+# generate masks; TODO: add validation (some nodes never seen?)
 
 masks = []
 for i in range(MASKCOUNT):
@@ -139,7 +145,9 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("Using device " + str(device))
 
 
-loader = DataLoader(dataset_plain, batch_size = BATCHSIZE, shuffle = True, pin_memory = True)
+loader = DataLoader(trainset, batch_size = BATCHSIZE, shuffle = True, pin_memory = True)
+validationloader = DataLoader(validationset, batch_size = BATCHSIZE, shuffle = True, pin_memory = True)
+testloader = DataLoader(testset, batch_size = BATCHSIZE, shuffle = True, pin_memory = True)
 
 model = Net(network.edge_index.to(device), network.num_nodes).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
@@ -150,15 +158,18 @@ for epoch in range(EPOCHS):
         model.train()
         optimizer.zero_grad()
         out = model(batch)
+        # chose a random mask from the set of masks
         currentmask = masks[randint(0, MASKCOUNT-1)]
         trainmask = currentmask[0] # the trainmask
         trainmask = trainmask[0:len(batch)]
         loss = F.mse_loss(out[trainmask], batch[trainmask])
         loss.backward()
         optimizer.step()
-    if epoch % 10 == 0:
-        out = model(batch)
-        testmask = testmask[0:len(batch)]
-        testloss = F.mse_loss(out[testmask], batch[testmask])
-        print("Epoch " + str(epoch) + ": " + str(testloss))
+    if epoch % 2 == 0:
+        validationloss = 0.0
+        for valbatch in validationloader:
+            valbatch = batch.to(device)
+            vout = model(valbatch)
+            validationloss += F.mse_loss(out, batch)
+        print("Epoch " + str(epoch) + ": " + str(validationloss))
 
